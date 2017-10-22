a41fcd3459c7db80c698fe6812bf40168e5cec7f
nghia <phamthanhnghia731997@gmail.com>
2017-10-22 09:19:07 +0700

    add dto Losanpham.java

---
+++ src/DTO/LoSanPham.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.sql.Timestamp;
+/**
+ *
+ * @author nghia
+ */
+public class LoSanPham {
+    public int id_lo_sp;
+    public Timestamp hsd;
+    public Timestamp nsx;
+    public int id_exist;
+    public int id_ton_kho;
+    public int id_phieu_nhap;
+
+    public LoSanPham(int id_lo_sp, Timestamp hsd, Timestamp nsx, int id_exist, int id_ton_kho, int id_phieu_nhap) {
+        this.id_lo_sp = id_lo_sp;
+        this.hsd = hsd;
+        this.nsx = nsx;
+        this.id_exist = id_exist;
+        this.id_ton_kho = id_ton_kho;
+        this.id_phieu_nhap = id_phieu_nhap;
+    }
+    
+    
+    
+}
