605b184edc33cd5d26bab25fae41612c7ea229e2
VinhNguyen309 <vanvinh309uit@gmail.com>
2017-11-17 13:51:14 +0700

    fThemSanPham fix

--- build/classes/Views/Test.rs
+++
@@ -1,8 +0,0 @@
-Views.SuggestionLabel$1
-Views.Test
-Views.SuggestionLabel$2
-Views.AutoSuggestor
-Views.AutoSuggestor$3
-Views.AutoSuggestor$2
-Views.SuggestionLabel
-Views.AutoSuggestor$1
--- src/DAO/daoSanPham.java
+++ src/DAO/daoSanPham.java
@@ -101,6 +101,34 @@ public class daoSanPham {
         
         return sanphamList;
     }
+    public ArrayList<SanPham> getListSanPhamTheoID(String id_sp)
+    {
+        ArrayList<SanPham> sanphamList = new ArrayList<>();
+        ArrayList<Object> arr = new ArrayList<>();
+        String searchQuery = "SELECT * FROM `San_pham` WHERE id_sp ='%"+id_sp+"%'";
+        try{
+            DataProvider.getIntance().open();
+            ResultSet rs = DataProvider.getIntance().excuteQuery(searchQuery, arr);          
+            SanPham sanpham;
+            
+            while(rs.next())
+            {
+                sanpham = new SanPham(
+                                rs.getInt("id_sp"),
+                                rs.getString("ten_sp"),
+                                rs.getString("hinh_anh").getBytes(),
+                                rs.getInt("id_exist"),
+                                rs.getInt("id_loai_sp")
+                                );
+                sanphamList.add(sanpham);
+            }
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return sanphamList;
+    }
     public boolean insertSanPham(String ten_sp,byte[] hinh_anh,int id_exist,int id_loai_sp) {
         String query = "INSERT INTO `San_pham`(`ten_sp`, `hinh_anh`, `id_exist`, `id_loai_sp`) VALUES ('%"+ten_sp+"%','%"+hinh_anh.toString()+"%','%"+id_exist+"%','%"+id_loai_sp+"%')";
         ArrayList<Object> arr = new ArrayList<>();
