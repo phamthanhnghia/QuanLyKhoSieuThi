2e7be690644d6f2dd6f5db8554d0546807997218
DinhTien24 <15520880@gm.uit.edu.vn>
2017-11-22 00:13:17 +0700

    SanPham on KiemKe

--- src/DAO/daoKiemKe.java
+++ src/DAO/daoKiemKe.java
@@ -5,10 +5,43 @@
  */
 package DAO;
 
+import DTO.SanPham;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Dinh Tien
  */
 public class daoKiemKe {
-    
+     private static daoKiemKe instance;
+
+    public static daoKiemKe getInstance() {
+        if(instance==null)instance=new daoKiemKe();
+        return instance;
+    }
+    public daoKiemKe() {
+    }
+    //
+    public ArrayList<SanPham> getListSanPham()
+    {
+        ArrayList<SanPham> result = new ArrayList<>();
+        String query="select * from San_pham";
+        ArrayList<Object> arr = new ArrayList<>();
+        try{
+        DataProvider.getIntance().open();
+        ResultSet rs = DataProvider.getIntance().excuteQuery(query, arr);
+        while(rs.next())
+        {
+            result.add(new SanPham(rs.getInt("id_sp"),rs.getString("ten_sp"),rs.getBytes("hinh_anh"),rs.getInt("id_exist"),rs.getInt("id_loai_sp")));
+        }
+        
+        DataProvider.getIntance().close();
+        }catch(SQLException ex){
+            DataProvider.getIntance().displayError(ex);
+        }
+        
+        return result;
+    }
 }
