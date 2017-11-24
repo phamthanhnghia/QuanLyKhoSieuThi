659d156f4f0bc67bfd8aa52d58f201c35673ff60
DinhTien24 <15520880@gm.uit.edu.vn>
2017-11-24 20:48:10 +0700

    dao getlisttrakho

--- src/DAO/daoTraNhaCungCap.java
+++ src/DAO/daoTraNhaCungCap.java
@@ -5,10 +5,48 @@
  */
 package DAO;
 
+import DTO.PhieuTraKho;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Dinh Tien
  */
 public class daoTraNhaCungCap {
-    
+    private static daoTraNhaCungCap instance;
+
+    public static daoTraNhaCungCap getInstance() {
+        if(instance==null)instance=new daoTraNhaCungCap();
+        return instance;
+    }
+
+    public daoTraNhaCungCap() {
+    }
+    public  ArrayList<PhieuTraKho> getListTraKho()
+    {
+         ArrayList<PhieuTraKho> result = new ArrayList<>();
+        String query="select * from Phieu_xuat_kho";
+        ArrayList<Object> arr = new ArrayList<>();
+        try{
+        DataProvider.getIntance().open();
+        ResultSet rs = DataProvider.getIntance().excuteQuery(query, arr);
+        while(rs.next())
+        {
+            result.add(new PhieuTraKho(rs.getInt("id_phieu_tra_kho"),
+                    rs.getString("thoi_gian_tra"),
+                    rs.getInt("sl_san_pham"),
+                    rs.getString("hinh_thuc_tra"),
+                    rs.getInt("id_lo_sp"),
+                    rs.getInt("id_nv")));
+        }
+        
+        DataProvider.getIntance().close();
+        }catch(SQLException ex){
+            DataProvider.getIntance().displayError(ex);
+        }
+        
+        return result;
+    }
 }
--- src/DTO/PhieuTraKho.java
+++ src/DTO/PhieuTraKho.java
@@ -12,17 +12,19 @@ package DTO;
 public class PhieuTraKho {
     public int id_phieu_tra_kho;
     public String thoi_gian_tra;
+    int sl_san_pham;
     public String hinh_thuc_tra;
     public int id_lo_sp;
     public int id_nv;
     public PhieuTraKho(){}
-    public PhieuTraKho(int id, String thoigian, String hinhthuc, int idlo, int idnv)
+    public PhieuTraKho(int id, String thoigian,int sl, String hinhthuc, int idlo, int idnv)
     {
         this.hinh_thuc_tra=hinhthuc;
         this.id_lo_sp=idlo;
         this.id_nv=idnv;
         this.id_phieu_tra_kho=id;
         this.thoi_gian_tra=thoigian;
+        this.sl_san_pham=sl;
     }
     
 }
