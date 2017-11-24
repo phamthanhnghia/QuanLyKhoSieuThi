a4f5fcd832e63492e187feda0eff616db5f8ce69
DinhTien24 <15520880@gm.uit.edu.vn>
2017-11-24 20:42:13 +0700

    DTO phieu tra kho

---
+++ src/DTO/PhieuTraKho.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author Dinh Tien
+ */
+public class PhieuTraKho {
+    public int id_phieu_tra_kho;
+    public String thoi_gian_tra;
+    public String hinh_thuc_tra;
+    public int id_lo_sp;
+    public int id_nv;
+    public PhieuTraKho(){}
+    public PhieuTraKho(int id, String thoigian, String hinhthuc, int idlo, int idnv)
+    {
+        this.hinh_thuc_tra=hinhthuc;
+        this.id_lo_sp=idlo;
+        this.id_nv=idnv;
+        this.id_phieu_tra_kho=id;
+        this.thoi_gian_tra=thoigian;
+    }
+    
+}
