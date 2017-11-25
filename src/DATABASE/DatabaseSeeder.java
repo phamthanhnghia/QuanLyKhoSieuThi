35a26b6bb7193c7a552231b6defc4662ac3d0f87
phamthanhnghia <phamthanhnghia731997@gmail.com>
2017-11-25 11:05:45 +0700

    update data

--- src/DATABASE/DatabaseSeeder.java
+++ src/DATABASE/DatabaseSeeder.java
@@ -16,7 +16,7 @@ import java.util.Random;
  */
 public class DatabaseSeeder {
     public static void main(String[] args){
-        for(int i=0 ; i< 20; i++){
+        for(int i=0 ; i< 40; i++){
             RanDomNhapKho("2016");
             RanDomNhapKho("2017");
         }
