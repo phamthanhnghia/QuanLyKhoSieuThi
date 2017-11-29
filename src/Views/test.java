2f7d409ecb031beb37007d729a07a1798a25675c
phamthanhnghia <phamthanhnghia731997@gmail.com>
2017-11-30 02:23:31 +0700

    reload

--- src/Views/test.java
+++ src/Views/test.java
@@ -5,6 +5,18 @@
  */
 package Views;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  *
  * @author admin
@@ -27,31 +39,79 @@ public class test extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jComboBox1 = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(115, 115, 115)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(229, Short.MAX_VALUE))
+                .addGap(131, 131, 131)
+                .addComponent(jButton1)
+                .addContainerGap(196, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(74, 74, 74)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(206, Short.MAX_VALUE))
+                .addGap(83, 83, 83)
+                .addComponent(jButton1)
+                .addContainerGap(194, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        XSSFWorkbook workbook = new XSSFWorkbook(); 
+      XSSFSheet spreadsheet = workbook.createSheet("cell types");
+      XSSFRow row = spreadsheet.createRow((short) 2);
+      row.createCell(0).setCellValue("Type of Cell");
+      row.createCell(1).setCellValue("cell value");
+      row = spreadsheet.createRow((short) 3);
+      row.createCell(0).setCellValue("set cell type BLANK");
+      row.createCell(1);
+      row = spreadsheet.createRow((short) 4);
+      row.createCell(0).setCellValue("set cell type BOOLEAN");
+      row.createCell(1).setCellValue(true);
+      row = spreadsheet.createRow((short) 5);
+      row.createCell(0).setCellValue("set cell type ERROR");
+      row.createCell(1).setCellValue(XSSFCell.CELL_TYPE_ERROR );
+      row = spreadsheet.createRow((short) 6);
+      row.createCell(0).setCellValue("set cell type date");
+      row.createCell(1).setCellValue(new Date());
+      row = spreadsheet.createRow((short) 7);
+      row.createCell(0).setCellValue("set cell type numeric" );
+      row.createCell(1).setCellValue(20 );
+      row = spreadsheet.createRow((short) 8);
+      row.createCell(0).setCellValue("set cell type string");
+      row.createCell(1).setCellValue("A String");
+      FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(new File("typesofcells.xlsx"));
+            try {
+                workbook.write(out);
+            } catch (IOException ex) {
+                Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+      System.out.println(
+      "typesofcells.xlsx written successfully");
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -89,6 +149,6 @@ public class test extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
