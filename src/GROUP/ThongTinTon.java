7c4516ed04a1bb300f094adf66b3ed3ed3c7e752
DinhTien24 <15520880@gm.uit.edu.vn>
2017-11-30 12:33:21 +0700

    Update Thong Tin ton

--- src/GROUP/ThongTinTon.java
+++ src/GROUP/ThongTinTon.java
@@ -24,23 +24,19 @@ public class ThongTinTon {
     public int id_chi_tiet_sp;
     public int so_luong_sp;
     public int so_tien_sp;
+    public int id_ton;
+    public int sl_sp;
     // nhan vien
     public String ten_nv;
 
     public ThongTinTon() {
     }
 
-    public ThongTinTon(String ten_sp, String ten_loai_sp, String dvt, int id_lo_sp, String hsd, String nsx, int id_ton_kho, int id_chi_tiet_sp, int so_luong_sp, int so_tien_sp, String ten_nv) {
+    public ThongTinTon(int id_lo_sp, int id_ton, String ten_sp, String hsd, int sl_sp) {
         this.ten_sp = ten_sp;
-        this.ten_loai_sp = ten_loai_sp;
-        this.dvt = dvt;
+        this.id_ton=id_ton;
         this.id_lo_sp = id_lo_sp;
         this.hsd = hsd;
-        this.nsx = nsx;
-        this.id_ton_kho = id_ton_kho;
-        this.id_chi_tiet_sp = id_chi_tiet_sp;
-        this.so_luong_sp = so_luong_sp;
-        this.so_tien_sp = so_tien_sp;
-        this.ten_nv = ten_nv;
+        this.sl_sp=sl_sp;
     }
 }
