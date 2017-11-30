976a44574f5c6d236ab4f4a899d50bedfa3841bd
phamthanhnghia <phamthanhnghia731997@gmail.com>
2017-11-30 09:41:59 +0700

    Excel KhuVuc, NguonCungCap

--- src/DAO/daoKhuVuc.java
+++ src/DAO/daoKhuVuc.java
@@ -6,9 +6,20 @@
 package DAO;
 
 import DTO.KhuVuc;
+import Views.fNhacungcap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 /**
  *
@@ -89,5 +100,55 @@ public class daoKhuVuc {
         
         return result;
      }
-     
+     public void ExcelKhuVuc(ArrayList<KhuVuc> arr){
+         HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("KhuVuc");
+        int rownum = 0;
+        Cell cell;
+        Row row;
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(0);
+        cell.setCellValue("id khu vực");
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(1);
+        cell.setCellValue("Tên Khu Vực");
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(2);
+        cell.setCellValue("Vị trí");
+        //
+        
+        
+        for(int i=0;i< arr.size() ; i++){
+            rownum++;
+            row = sheet.createRow(rownum);
+            //
+            cell = row.createCell(0);
+            cell.setCellValue(arr.get(i).id_khu_vuc);
+            //
+            cell = row.createCell(1);
+            cell.setCellValue(arr.get(i).ten_khu_vuc);
+            //
+            cell = row.createCell(2);
+            cell.setCellValue(arr.get(i).vi_tri);
+            //
+            
+        }
+        File file = new File("C:/demo/khuvuc.xls");
+        file.getParentFile().mkdirs();
+ 
+        FileOutputStream outFile;
+        try {
+            outFile = new FileOutputStream(file);
+            workbook.write(outFile);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(fNhacungcap.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(fNhacungcap.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("Created file: " + file.getAbsolutePath());
+     }
 }
--- src/DAO/daoNguonCungCap.java
+++ src/DAO/daoNguonCungCap.java
@@ -5,11 +5,16 @@
  */
 package DAO;
 import DTO.ChiTietPhieuNhap;
+import DTO.KhuVuc;
 import DTO.NguonCungCap;
 import DTO.NhanVien;
 import DTO.XuatKho;
+import Views.fNhacungcap;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,7 +22,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 /**
  *
@@ -315,4 +326,66 @@ public class daoNguonCungCap {
         }
         return result;
     }
+    
+    public void ExcelNguonCung(ArrayList<NguonCungCap> arr){
+         HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("NguonCungCap");
+        int rownum = 0;
+        Cell cell;
+        Row row;
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(0);
+        cell.setCellValue("Tên nhà cung cấp");
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(1);
+        cell.setCellValue("Tên Đại Diện");
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(2);
+        cell.setCellValue("SĐT");
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(3);
+        cell.setCellValue("Địa Chỉ");
+        //
+        row = sheet.createRow(rownum);
+        cell = row.createCell(4);
+        cell.setCellValue("Email");
+        
+        for(int i=0;i< arr.size() ; i++){
+            rownum++;
+            row = sheet.createRow(rownum);
+            //
+            cell = row.createCell(0);
+            cell.setCellValue(arr.get(i).ten_nha_cc);
+            //
+            cell = row.createCell(1);
+            cell.setCellValue(arr.get(i).ten_dai_dien);
+            //
+            cell = row.createCell(2);
+            cell.setCellValue(arr.get(i).sdt);
+            //
+            cell = row.createCell(3);
+            cell.setCellValue(arr.get(i).dia_chi);
+            //
+            cell = row.createCell(4);
+            cell.setCellValue(arr.get(i).email);
+        }
+        File file = new File("C:/demo/nguoncungcap.xls");
+        file.getParentFile().mkdirs();
+ 
+        FileOutputStream outFile;
+        try {
+            outFile = new FileOutputStream(file);
+            workbook.write(outFile);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(fNhacungcap.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(fNhacungcap.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("Created file: " + file.getAbsolutePath());
+     }
 }
