e8195a1e757780f0c0a28dc3bbaec0f8791d6c48
DinhTien24 <15520880@gm.uit.edu.vn>
2017-12-04 15:08:49 +0700

    createlineChartTonKhoTheoThang

--- src/DAO/daoBaoCao.java
+++ src/DAO/daoBaoCao.java
@@ -22,40 +22,42 @@ import org.jfree.data.category.DefaultCategoryDataset;
  *
  * @author Pham Thanh Nghia
  */
-
 public class daoBaoCao {
-    
+
     private static daoBaoCao instance;
+
     public static daoBaoCao getInstance() {
-        if(instance==null)instance=new daoBaoCao();
+        if (instance == null) {
+            instance = new daoBaoCao();
+        }
         return instance;
     }
-    
-    public JPanel createChartPanelLoaiSanPham_jTreeChart(){
+
+    public JPanel createChartPanelLoaiSanPham_jTreeChart() {
         double tong_so_luong = 0;
         double du_thua = 0;
         ArrayList<LoaiSanPham_jTreeChart> arr = daoKho.getInstance().getListLoaiSanPham_jTreeChart();
         DefaultPieDataset pieDataset = new DefaultPieDataset();
-        if(!arr.isEmpty()){
-            for(int i=0;i<arr.size();i++){
-                tong_so_luong = tong_so_luong + (double)arr.get(i).so_luong;
+        if (!arr.isEmpty()) {
+            for (int i = 0; i < arr.size(); i++) {
+                tong_so_luong = tong_so_luong + (double) arr.get(i).so_luong;
             }
-            if(arr.size() <= 6){
-                for(int i = 0;i<arr.size();i++){
-                    double number = (arr.get(i).so_luong/tong_so_luong)*100;
+            if (arr.size() <= 6) {
+                for (int i = 0; i < arr.size(); i++) {
+                    double number = (arr.get(i).so_luong / tong_so_luong) * 100;
                     pieDataset.setValue(arr.get(i).ten_loai_sp, number);
                 }
-            }else{
-                for(int i = 0;i < 6 ;i++){
+            } else {
+                for (int i = 0; i < 6; i++) {
                     du_thua = tong_so_luong - arr.get(i).so_luong;
-                    double number = (arr.get(i).so_luong/tong_so_luong)*100;
+                    double number = (arr.get(i).so_luong / tong_so_luong) * 100;
                     pieDataset.setValue(arr.get(i).ten_loai_sp, number);
                 }
-                double number = (du_thua/tong_so_luong)*10;
-                pieDataset.setValue("Loại khác", number );
+                double number = (du_thua / tong_so_luong) * 10;
+                pieDataset.setValue("Loại khác", number);
             }
-            
-        }else{
+
+        } else {
             pieDataset.setValue("Chưa có dữ liệu trong kho", new Integer(90));
             pieDataset.setValue("Chưa có gì ", new Integer(10));
 
@@ -63,27 +65,38 @@ public class daoBaoCao {
         JFreeChart chart = ChartFactory.createPieChart3D("Phần trăm theo loại sản phẩm trong kho", pieDataset, true, true, true);
         return new ChartPanel(chart);
     }
-    
-    public Long SoLuongLoTrongKhoHienTai(){
+
+    public Long SoLuongLoTrongKhoHienTai() {
         ArrayList<Kho> arrKho = daoKho.getInstance().getListKho();
-        long sum =0;
-        for(int i=0;i< arrKho.size();i++){
-            sum=sum + arrKho.get(i).sl_san_pham;
+        long sum = 0;
+        for (int i = 0; i < arrKho.size(); i++) {
+            sum = sum + arrKho.get(i).sl_san_pham;
         }
         return sum;
     }
-    public Long SoLuongTonKhoTheoNgay(String date)
-    {
+
+    public Long SoLuongTonKhoTheoNgay(String date) {
         ArrayList<ThongTinTon> arr = daoTonKho.getInstance().getTonKhoTheoNgay(date);
-        long sum =0;
-        if(arr.isEmpty()==false)
-        {
-        for(int i=0;i< arr.size();i++){
-            sum=sum + arr.get(i).sl_sp;
-        }
+        long sum = 0;
+        if (arr.isEmpty() == false) {
+            for (int i = 0; i < arr.size(); i++) {
+                sum = sum + arr.get(i).sl_sp;
+            }
         }
         return sum;
     }
+
+    public Long SoLuongTonKhoTheoThang(String thang, String nam) {
+        ArrayList<ThongTinTon> arr = daoTonKho.getInstance().getTonKhoTheoThang(thang, nam);
+        long sum = 0;
+        if (arr.isEmpty() == false) {
+            for (int i = 0; i < arr.size(); i++) {
+                sum = sum + arr.get(i).sl_sp;
+            }
+        }
+        return sum;
+    }
+
     public JPanel createlineChartTonKhoTheoNgay() {
         String TonKho = "";
         String ngay1 = DAO.DateTimeNow.getIntance().DateNow;
@@ -108,10 +121,50 @@ public class daoBaoCao {
         dataset.addValue(data2, TonKho, ngay2);
         dataset.addValue(data1, TonKho, ngay1);
 
-
         JFreeChart lineChart = ChartFactory.createLineChart3D(
                 "Số lượng sản phẩm tồn kho theo ngày",
                 "Ngày",
+                "Số lượng lô",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, false);
+        return new ChartPanel(lineChart);
+    }
+
+    public JPanel createlineChartTonKhoTheoThang() {
+        String TonKho = "";
+        String ngay = DAO.DateTimeNow.getIntance().DateNow;
+        String nam1=ngay.substring(0, 4);
+        String thang1 = ngay.substring(5, 7);
+        String thang2 =DAO.DateTimeNow.getIntance().getThangTruoc(thang1);
+        String thang3 =DAO.DateTimeNow.getIntance().getThangTruoc(thang2);
+        String thang4 =DAO.DateTimeNow.getIntance().getThangTruoc(thang3);
+        String thang5 =DAO.DateTimeNow.getIntance().getThangTruoc(thang4);
+        String thang6 =DAO.DateTimeNow.getIntance().getThangTruoc(thang5);
+        String nam2 = DAO.DateTimeNow.getIntance().getNam(thang1, nam1);
+        String nam3 = DAO.DateTimeNow.getIntance().getNam(thang2, nam2);
+        String nam4 = DAO.DateTimeNow.getIntance().getNam(thang3, nam3);
+        String nam5 = DAO.DateTimeNow.getIntance().getNam(thang4, nam4);
+        String nam6 = DAO.DateTimeNow.getIntance().getNam(thang5, nam5);
+        long data1 = DAO.daoBaoCao.getInstance().SoLuongTonKhoTheoThang(thang1, nam1);
+        long data2 = DAO.daoBaoCao.getInstance().SoLuongTonKhoTheoThang(thang2, nam2);
+        long data3 = DAO.daoBaoCao.getInstance().SoLuongTonKhoTheoThang(thang3, nam3);
+        long data4 = DAO.daoBaoCao.getInstance().SoLuongTonKhoTheoThang(thang4, nam4);
+        long data5 = DAO.daoBaoCao.getInstance().SoLuongTonKhoTheoThang(thang5, nam5);
+        long data6 = DAO.daoBaoCao.getInstance().SoLuongTonKhoTheoThang(thang6, nam6);
+        
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(data6, TonKho, thang6+"/"+nam6);
+        dataset.addValue(data5, TonKho, thang5+"/"+nam5);
+        dataset.addValue(data4, TonKho, thang4+"/"+nam4);
+        dataset.addValue(data3, TonKho, thang3+"/"+nam3);
+        dataset.addValue(data2, TonKho, thang2+"/"+nam2);
+        dataset.addValue(data1, TonKho, thang1+"/"+nam1);
+
+        JFreeChart lineChart = ChartFactory.createLineChart3D(
+                "Số lượng sản phẩm tồn kho theo tháng",
+                "Tháng",
                 "Số lượng lô",
                 dataset,
                 PlotOrientation.VERTICAL,
